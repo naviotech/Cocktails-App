a9de3d4675c686d578bf89a424d9c70eb6c13596
Eduardo Navío <naviomaya@gmail.com>
2024-07-17 17:49:44 +0200

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
-
+import { useLocation } from "react-router-dom"
 
 const Footer = () => {
+  const { pathname } = useLocation()
   return (
     <>
-      <footer className=" w-full bg-slate-800 flex flex-col justify-center items-center mt-16 md:mt-28">
-        <article className="px-6 py-10 flex items-center justify-center max-w-screen-xl w-full md:py-20">
+      <footer className="w-full bg-slate-800 flex flex-col justify-center items-center mt-16 md:mt-28 ">
+        <article className={pathname=== "/" ? "px-6 py-10 flex items-center justify-center max-w-screen-xl w-full md:py-20": "px-6 py-10 flex items-center justify-center max-w-screen-xl w-full "}>
           <figure>
             <img className="w-16" src="/svg/logo.svg"></img>
           </figure>
--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -5,9 +5,9 @@ import Footer from "../../components/Footer"
 const Favourites = () => {
   
   return (
-    <>
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <Header/>
-      <main className="mt-10 flex flex-col justify-center items-center">
+      <main className="mt-10 flex flex-col justify-center items-center flex-grow">
         <section className=" w-full px-6 lg:px-12 py-10 flex flex-col justify-center items-center max-w-screen-xl">
           <h1 className="text-center font-bold text-3xl mb-12 sm:mb-24">Favorite Drinks</h1>
           <RecipesFav/>
@@ -15,7 +15,7 @@ const Favourites = () => {
         
       </main>
       <Footer/>
-    </>
+    </div>
   )
 }
 
